ed6853e660b5bc3482d195757266844b9a4cc371
mattme94 <39651363+mattme94@users.noreply.github.com>
2019-05-02 23:30:45 -0400

    Added batch regularizer to all layers and removed the third layer from the actor model:
    mean=-74.21311225246403
    max=15.87
    min=-8812
    mean_last_10=-13.024807197387306
    mean_last_100=-13.035521177703933

--- agents/agent.py
+++ agents/agent.py
@@ -65,8 +65,6 @@ class Actor:
         net = layers.BatchNormalization()(net)
         net = layers.Dense(units=64, activation='relu', use_bias=False)(net)
         net = layers.BatchNormalization()(net)
-        net = layers.Dense(units=32, activation='relu', use_bias=False)(net)
-        net = layers.BatchNormalization()(net)
 
         # Try different layer sizes, activations, add batch normalization, regularizers, etc.
 
@@ -120,12 +118,14 @@ class Critic:
         actions = layers.Input(shape=(self.action_size,), name='actions')
 
         # Add hidden layer(s) for state pathway
-        net_states = layers.Dense(units=32, activation='relu')(states)
+        net_states = layers.Dense(units=32, activation='relu', use_bias=False)(states)
+        net_states = layers.BatchNormalization()(net_states)
         net_states = layers.Dense(units=64, activation='relu', use_bias=False)(net_states)
         net_states = layers.BatchNormalization()(net_states)
 
         # Add hidden layer(s) for action pathway
-        net_actions = layers.Dense(units=32, activation='relu')(actions)
+        net_actions = layers.Dense(units=32, activation='relu', use_bias=False)(actions)
+        net_actions = layers.BatchNormalization()(net_actions)
         net_actions = layers.Dense(units=64, activation='relu', use_bias=False)(net_actions)
         net_actions = layers.BatchNormalization()(net_actions)
 
@@ -136,10 +136,6 @@ class Critic:
         net = layers.Activation('relu')(net)
 
         # Add more layers to the combined network if needed
-        net = layers.Dense(units=32, activation='relu')(net)
-        net = layers.Dropout(0.5)(net)
-        net = layers.Dense(units=64, activation='relu')(net)
-        net = layers.Dropout(0.5)(net)
 
         # Add final output layer to prduce action values (Q values)
         Q_values = layers.Dense(units=1, name='q_values')(net)
